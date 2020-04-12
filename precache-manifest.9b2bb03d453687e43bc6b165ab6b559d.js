fa615397e4d0c3bc69c8297d1f086c3465f61783
Saljo <saljo@dexlock.com>
2020-04-12 11:57:58 +0530

    Updates

---
+++ precache-manifest.9b2bb03d453687e43bc6b165ab6b559d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd480967d5c4ae1e0840c9267ea63e6",
+    "url": "/timer-app/index.html"
+  },
+  {
+    "revision": "fa5f4ace86e36417f2e5",
+    "url": "/timer-app/static/css/2.1417c86a.chunk.css"
+  },
+  {
+    "revision": "db18e4755c3459eeb299",
+    "url": "/timer-app/static/css/main.fbf7ac9b.chunk.css"
+  },
+  {
+    "revision": "fa5f4ace86e36417f2e5",
+    "url": "/timer-app/static/js/2.77be2a28.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/timer-app/static/js/2.77be2a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db18e4755c3459eeb299",
+    "url": "/timer-app/static/js/main.f13c4281.chunk.js"
+  },
+  {
+    "revision": "662f82e563e9e56fbe73",
+    "url": "/timer-app/static/js/runtime-main.445d8d6a.js"
+  },
+  {
+    "revision": "e294ff69258ec4aadf0ca0771b586179",
+    "url": "/timer-app/static/media/alarm.e294ff69.svg"
+  },
+  {
+    "revision": "b17a93ee57e46f7234b3c7ec5c290ecc",
+    "url": "/timer-app/static/media/delete.b17a93ee.svg"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/timer-app/static/media/edit.2927f8de.svg"
+  },
+  {
+    "revision": "d033a43761bd03260234e8088de7f170",
+    "url": "/timer-app/static/media/event.d033a437.svg"
+  }
+]);
